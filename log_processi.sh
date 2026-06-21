fe35a17e2add5a51b171db2f5f09b5cefe4c3311
giuseppepampapimentel <pampapimentelg@gmail.com>
2026-06-21 18:46:17 +0200

    log_processi.sh

--- log_processi.sh
+++ log_processi.sh
@@ -1,1 +1,11 @@
+# Utenti da monitorare
+USERS=("alice" "marco" "giulia")
+# Log file
+LOGFILE="/var/log/dev_processi.log"
+echo "----- $(date '+%Y-%m-%d %H:%M:%S') -----" >> "$LOGFILE"
 
+for user in "${USERS[@]}"; do
+    echo "Processi dell'utente $user:" >> "$LOGFILE"
+    ps -u "$user" >> "$LOGFILE"
+    echo "" >> "$LOGFILE"
+done
