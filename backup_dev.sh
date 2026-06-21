588ba6aa1a7a9871042c16d7e30647f4877799b9
giuseppepampapimentel <pampapimentelg@gmail.com>
2026-06-21 18:43:50 +0200

    Update backup_dev.sh

--- backup_dev.sh
+++ backup_dev.sh
@@ -1,1 +1,12 @@
-
+# Cartella da salvare
+SOURCE="/srv/dev_shared"
+# Cartella di destinazione
+DEST="/srv/backups"
+# Nome del file con la data ed ora
+FILENAME="backup_$(date +%Y-%m-%d_%H-%M-%S).tar.gz"
+# Log file
+LOGFILE="/var/log/dev_backup.log"
+# Fa l'esecuzione del backup
+tar -czf "$DEST/$FILENAME" "$SOURCE"
+# Scrive nel log
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Backup eseguito: $FILENAME" >> "$LOGFILE"
